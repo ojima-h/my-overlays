9d6a00e719c28dcee3469eadf6ae465ed83f9f0e
Ojima Hikaru <ojiam.h@gmail.com>
2012-05-03 13:32:36 +0900

    edit site lisp

--- app-emacs/rdefs-el/files/50rdefs-el-gentoo.el
+++ app-emacs/rdefs-el/files/50rdefs-el-gentoo.el
@@ -2,3 +2,10 @@
 (autoload 'ruby-defs "rdefs-el")
 ;(autoload 'rdefs-mode "rdefs-el"
 ;            "Minor mode for ruby defs" t)
+
+;;; ruby-mode key-bindings (extracted and modified from the original)
+;;; Visit a ruby source code file and press Meta(ESC)-e, and you get
+;;; the output from rdefs command.
+
+;;(setq ruby-mode-map (make-sparse-keymap))
+(define-key ruby-mode-map "\ee" 'ruby-defs)
