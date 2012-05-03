cf4456f643a361291d03de28a61845ee49ad35e4
Ojima Hikaru <ojiam.h@gmail.com>
2012-05-03 13:49:34 +0900

    save

--- app-emacs/rdefs-el/files/50rdefs-el-gentoo.el
+++ app-emacs/rdefs-el/files/50rdefs-el-gentoo.el
@@ -1,5 +1,5 @@
 (add-to-list 'load-path "@SITELISP@")
-(autoload 'ruby-defs "rdefs-el")
+(autoload 'ruby-defs "rdefs")
 ;(autoload 'rdefs-mode "rdefs-el"
 ;            "Minor mode for ruby defs" t)
 
