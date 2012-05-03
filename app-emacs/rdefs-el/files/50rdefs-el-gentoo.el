42a093439f2d35e7f7bf12431c2aa996e9e1f0a7
Ojima Hikaru <ojiam.h@gmail.com>
2012-05-03 11:12:47 +0900

    edit sitelisp

--- app-emacs/rdefs-el/files/50rdefs-el-gentoo.el
+++ app-emacs/rdefs-el/files/50rdefs-el-gentoo.el
@@ -1,42 +1,8 @@
-;; some code to use rdefs from emacs
-
-(defconst rdefs-buffer-name "*rdefs*")
-(defconst rdefs-command "rdefs")
-(setq rdefs-mode-keymap (make-sparse-keymap))
-(define-key rdefs-mode-keymap "q" '(lambda ()
-				     (interactive)
-				     (kill-buffer rdefs-buffer-name)))
-(define-minor-mode rdefs-mode
-  "Toggle Rdefs mode"
-  ;; The initial value.
-  nil
-  ;; The indicator for the mode line.
-  :lighter " Rdefs"
-  ;; The minor mode bindings.
-  :keymap rdefs-mode-keymap
-  :group 'rdefs)
-
-;;; extract-ruby-defs with rdefs
-(defun ruby-defs ()
-  (interactive)
-  (let ((oldbuf (current-buffer)))
-    (setq buf (get-buffer-create rdefs-buffer-name))
-    (set-buffer buf)
-    (erase-buffer)
-    (set-buffer oldbuf)
-    (call-process-region
-     (point-min)
-     (point-max) rdefs-command nil buf nil "-n")
-    (switch-to-buffer-other-window buf)
-;;    (toggle-read-only 1)
-    (ruby-mode)
-    (rdefs-mode)))
-
 ;;; ruby-mode key-bindings (extracted and modified from the original)
 ;;; Visit a ruby source code file and press Meta(ESC)-e, and you get
 ;;; the output from rdefs command.
 
-;; (setq ruby-mode-map (make-sparse-keymap))
+(setq ruby-mode-map (make-sparse-keymap))
 (define-key ruby-mode-map "\ee" 'ruby-defs)
 ;; (define-key ruby-mode-map "{" 'ruby-electric-brace)
 ;; (define-key ruby-mode-map "}" 'ruby-electric-brace)
