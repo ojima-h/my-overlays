e882a275d98d51d577b8a5d9d49d0713c32937c6
Ojima Hikaru <ojiam.h@gmail.com>
2012-05-08 05:45:11 +0900

    anything との衝突を回避しました
    disabled ido-mode

--- app-emacs/rinari/files/50rinari-gentoo.el
+++ app-emacs/rinari/files/50rinari-gentoo.el
@@ -1,6 +1,6 @@
 ;; Interactively Do Things (highly recommended, but not strictly required)
-(require 'ido)
-(ido-mode t)
+;(require 'ido)
+;(ido-mode t)
 
 ;; Rinari
 (add-to-list 'load-path "@SITELISP@")
